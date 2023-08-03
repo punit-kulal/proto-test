2cdfe00bfb0434a52aff832f426bf45fc0ac40e5
Punit Kulal <punitkulal1996@gmail.com>
2023-08-03 17:40:12 +0530

    add new protos

--- js/protos.js
+++ js/protos.js
@@ -960,4 +960,325 @@ $root.test = (function() {
     return test;
 })();
 
+$root.xyz = (function() {
+
+    /**
+     * Namespace xyz.
+     * @exports xyz
+     * @namespace
+     */
+    var xyz = {};
+
+    xyz.v1 = (function() {
+
+        /**
+         * Namespace v1.
+         * @memberof xyz
+         * @namespace
+         */
+        var v1 = {};
+
+        v1.Xyz = (function() {
+
+            /**
+             * Properties of a Xyz.
+             * @memberof xyz.v1
+             * @interface IXyz
+             * @property {string|null} [name] Xyz name
+             * @property {number|null} [id] Xyz id
+             * @property {string|null} [email] Xyz email
+             */
+
+            /**
+             * Constructs a new Xyz.
+             * @memberof xyz.v1
+             * @classdesc Represents a Xyz.
+             * @implements IXyz
+             * @constructor
+             * @param {xyz.v1.IXyz=} [properties] Properties to set
+             */
+            function Xyz(properties) {
+                if (properties)
+                    for (var keys = Object.keys(properties), i = 0; i < keys.length; ++i)
+                        if (properties[keys[i]] != null)
+                            this[keys[i]] = properties[keys[i]];
+            }
+
+            /**
+             * Xyz name.
+             * @member {string|null|undefined} name
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Xyz.prototype.name = null;
+
+            /**
+             * Xyz id.
+             * @member {number|null|undefined} id
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Xyz.prototype.id = null;
+
+            /**
+             * Xyz email.
+             * @member {string|null|undefined} email
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Xyz.prototype.email = null;
+
+            // OneOf field names bound to virtual getters and setters
+            var $oneOfFields;
+
+            /**
+             * Xyz _name.
+             * @member {"name"|undefined} _name
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Object.defineProperty(Xyz.prototype, "_name", {
+                get: $util.oneOfGetter($oneOfFields = ["name"]),
+                set: $util.oneOfSetter($oneOfFields)
+            });
+
+            /**
+             * Xyz _id.
+             * @member {"id"|undefined} _id
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Object.defineProperty(Xyz.prototype, "_id", {
+                get: $util.oneOfGetter($oneOfFields = ["id"]),
+                set: $util.oneOfSetter($oneOfFields)
+            });
+
+            /**
+             * Xyz _email.
+             * @member {"email"|undefined} _email
+             * @memberof xyz.v1.Xyz
+             * @instance
+             */
+            Object.defineProperty(Xyz.prototype, "_email", {
+                get: $util.oneOfGetter($oneOfFields = ["email"]),
+                set: $util.oneOfSetter($oneOfFields)
+            });
+
+            /**
+             * Creates a new Xyz instance using the specified properties.
+             * @function create
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {xyz.v1.IXyz=} [properties] Properties to set
+             * @returns {xyz.v1.Xyz} Xyz instance
+             */
+            Xyz.create = function create(properties) {
+                return new Xyz(properties);
+            };
+
+            /**
+             * Encodes the specified Xyz message. Does not implicitly {@link xyz.v1.Xyz.verify|verify} messages.
+             * @function encode
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {xyz.v1.IXyz} message Xyz message or plain object to encode
+             * @param {$protobuf.Writer} [writer] Writer to encode to
+             * @returns {$protobuf.Writer} Writer
+             */
+            Xyz.encode = function encode(message, writer) {
+                if (!writer)
+                    writer = $Writer.create();
+                if (message.name != null && Object.hasOwnProperty.call(message, "name"))
+                    writer.uint32(/* id 1, wireType 2 =*/10).string(message.name);
+                if (message.id != null && Object.hasOwnProperty.call(message, "id"))
+                    writer.uint32(/* id 2, wireType 0 =*/16).int32(message.id);
+                if (message.email != null && Object.hasOwnProperty.call(message, "email"))
+                    writer.uint32(/* id 3, wireType 2 =*/26).string(message.email);
+                return writer;
+            };
+
+            /**
+             * Encodes the specified Xyz message, length delimited. Does not implicitly {@link xyz.v1.Xyz.verify|verify} messages.
+             * @function encodeDelimited
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {xyz.v1.IXyz} message Xyz message or plain object to encode
+             * @param {$protobuf.Writer} [writer] Writer to encode to
+             * @returns {$protobuf.Writer} Writer
+             */
+            Xyz.encodeDelimited = function encodeDelimited(message, writer) {
+                return this.encode(message, writer).ldelim();
+            };
+
+            /**
+             * Decodes a Xyz message from the specified reader or buffer.
+             * @function decode
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {$protobuf.Reader|Uint8Array} reader Reader or buffer to decode from
+             * @param {number} [length] Message length if known beforehand
+             * @returns {xyz.v1.Xyz} Xyz
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {$protobuf.util.ProtocolError} If required fields are missing
+             */
+            Xyz.decode = function decode(reader, length) {
+                if (!(reader instanceof $Reader))
+                    reader = $Reader.create(reader);
+                var end = length === undefined ? reader.len : reader.pos + length, message = new $root.xyz.v1.Xyz();
+                while (reader.pos < end) {
+                    var tag = reader.uint32();
+                    switch (tag >>> 3) {
+                    case 1: {
+                            message.name = reader.string();
+                            break;
+                        }
+                    case 2: {
+                            message.id = reader.int32();
+                            break;
+                        }
+                    case 3: {
+                            message.email = reader.string();
+                            break;
+                        }
+                    default:
+                        reader.skipType(tag & 7);
+                        break;
+                    }
+                }
+                return message;
+            };
+
+            /**
+             * Decodes a Xyz message from the specified reader or buffer, length delimited.
+             * @function decodeDelimited
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {$protobuf.Reader|Uint8Array} reader Reader or buffer to decode from
+             * @returns {xyz.v1.Xyz} Xyz
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {$protobuf.util.ProtocolError} If required fields are missing
+             */
+            Xyz.decodeDelimited = function decodeDelimited(reader) {
+                if (!(reader instanceof $Reader))
+                    reader = new $Reader(reader);
+                return this.decode(reader, reader.uint32());
+            };
+
+            /**
+             * Verifies a Xyz message.
+             * @function verify
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {Object.<string,*>} message Plain object to verify
+             * @returns {string|null} `null` if valid, otherwise the reason why it is not
+             */
+            Xyz.verify = function verify(message) {
+                if (typeof message !== "object" || message === null)
+                    return "object expected";
+                var properties = {};
+                if (message.name != null && message.hasOwnProperty("name")) {
+                    properties._name = 1;
+                    if (!$util.isString(message.name))
+                        return "name: string expected";
+                }
+                if (message.id != null && message.hasOwnProperty("id")) {
+                    properties._id = 1;
+                    if (!$util.isInteger(message.id))
+                        return "id: integer expected";
+                }
+                if (message.email != null && message.hasOwnProperty("email")) {
+                    properties._email = 1;
+                    if (!$util.isString(message.email))
+                        return "email: string expected";
+                }
+                return null;
+            };
+
+            /**
+             * Creates a Xyz message from a plain object. Also converts values to their respective internal types.
+             * @function fromObject
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {Object.<string,*>} object Plain object
+             * @returns {xyz.v1.Xyz} Xyz
+             */
+            Xyz.fromObject = function fromObject(object) {
+                if (object instanceof $root.xyz.v1.Xyz)
+                    return object;
+                var message = new $root.xyz.v1.Xyz();
+                if (object.name != null)
+                    message.name = String(object.name);
+                if (object.id != null)
+                    message.id = object.id | 0;
+                if (object.email != null)
+                    message.email = String(object.email);
+                return message;
+            };
+
+            /**
+             * Creates a plain object from a Xyz message. Also converts values to other types if specified.
+             * @function toObject
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {xyz.v1.Xyz} message Xyz
+             * @param {$protobuf.IConversionOptions} [options] Conversion options
+             * @returns {Object.<string,*>} Plain object
+             */
+            Xyz.toObject = function toObject(message, options) {
+                if (!options)
+                    options = {};
+                var object = {};
+                if (message.name != null && message.hasOwnProperty("name")) {
+                    object.name = message.name;
+                    if (options.oneofs)
+                        object._name = "name";
+                }
+                if (message.id != null && message.hasOwnProperty("id")) {
+                    object.id = message.id;
+                    if (options.oneofs)
+                        object._id = "id";
+                }
+                if (message.email != null && message.hasOwnProperty("email")) {
+                    object.email = message.email;
+                    if (options.oneofs)
+                        object._email = "email";
+                }
+                return object;
+            };
+
+            /**
+             * Converts this Xyz to JSON.
+             * @function toJSON
+             * @memberof xyz.v1.Xyz
+             * @instance
+             * @returns {Object.<string,*>} JSON object
+             */
+            Xyz.prototype.toJSON = function toJSON() {
+                return this.constructor.toObject(this, $protobuf.util.toJSONOptions);
+            };
+
+            /**
+             * Gets the default type url for Xyz
+             * @function getTypeUrl
+             * @memberof xyz.v1.Xyz
+             * @static
+             * @param {string} [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+             * @returns {string} The default type url
+             */
+            Xyz.getTypeUrl = function getTypeUrl(typeUrlPrefix) {
+                if (typeUrlPrefix === undefined) {
+                    typeUrlPrefix = "type.googleapis.com";
+                }
+                return typeUrlPrefix + "/xyz.v1.Xyz";
+            };
+
+            return Xyz;
+        })();
+
+        return v1;
+    })();
+
+    return xyz;
+})();
+
 module.exports = $root;
