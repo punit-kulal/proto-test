e8bab9b626cc1da998b69876ac1fcdb406c77d33
Punit Kulal <punitkulal1996@gmail.com>
2023-08-18 11:56:53 +0530

    updated to python compiler

---
+++ protocol-buffers/python_protos/xyz/v1/xyz_pb2.pyi
@@ -0,0 +1,15 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Xyz(_message.Message):
+    __slots__ = ["name", "id", "email"]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    EMAIL_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    id: int
+    email: str
+    def __init__(self, name: _Optional[str] = ..., id: _Optional[int] = ..., email: _Optional[str] = ...) -> None: ...
